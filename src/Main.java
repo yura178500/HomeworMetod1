c845aea3835e0fbd8ba9fc5f019bd557a06d50e7
yura178500 <yura178500@yandex.ru>
2022-10-23 15:29:45 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main (String[]args){
+
+        int year = 2024;
+        if ((year % 4 == 0) && year % 100 != 0) {
+            System.out.println(year + "   Весокосный год");
+        } else if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+            System.out.println(year + "   Весокосный год");
+        } else {
+            System.out.println(year + "   Не весокосный год");
+        }
+    }
+    }
